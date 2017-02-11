78c8f6eb1eb07a0deaa118ac15b709b08c044491
Atalya Drescher <Atalya Drescher>
2017-02-11 14:05:21 +0200

    Added permissions handling

--- app/src/main/java/com/example/gilad/videomanager/MainActivity.java
+++ app/src/main/java/com/example/gilad/videomanager/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.gilad.videomanager;
 
 import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -14,6 +17,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends BaseActivity {
     static final String FILTER = "Hearthstone";
@@ -23,8 +29,7 @@ public class MainActivity extends BaseActivity {
         Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
 
-    private VideosManager vm = new VideosManager(
-            Environment.getExternalStorageDirectory().getPath() + "/Movies", FILTER);
+    private VideosManager vm;
 
 
     @Override
@@ -37,6 +42,9 @@ public class MainActivity extends BaseActivity {
 
     private void populateList() {
 
+        vm = new VideosManager(
+                Environment.getExternalStorageDirectory().getPath() + "/Movies", FILTER);
+
         ListView listView = (ListView) findViewById(R.id.videoList);
 
         ListAdapter adapter = new CustomAdapter(this, vm.getVideos());
@@ -46,11 +54,41 @@ public class MainActivity extends BaseActivity {
 
     private void populateListIfPermitted()
     {
+        List<String> deniedPermissions = new ArrayList<String>();
 
+        for (String curr : permissionsArray) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, curr)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                deniedPermissions.add(curr);
+            }
+        }
+
+        if (deniedPermissions.size() > 0) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    deniedPermissions.toArray(new String[deniedPermissions.size()]), 0);
+        } else {
+            onRequestPermissionsResult(0, null, null);
+        }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        boolean bAcceptedAll = true;
+
+        // IT if it null - there are already permissions
+        if (permissions != null && grantResults != null) {
+
+            // Goes on each result and checks if it is granted
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED)
+                    bAcceptedAll = false;
+            }
+        }
+
+        if (bAcceptedAll)
+            populateList();
     }
 }
